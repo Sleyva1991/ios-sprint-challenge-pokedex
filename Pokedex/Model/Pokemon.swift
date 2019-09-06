f3b0de164f598879e8f9f0cb5d63fef91ec1b66b
Steven <Sleyva1991@outlook.com>
2019-09-06 14:13:32 -0400

    Started on the pokedexController

--- Pokedex/Model/Pokemon.swift
+++ Pokedex/Model/Pokemon.swift
@@ -6,4 +6,44 @@
 //  Copyright © 2019 Lambda School. All rights reserved.
 //
 
+import Foundation
 
+struct Pokemon: Codable {
+    let abilities: [Ability]
+    let type: [PokemonType]
+    let id: Int
+    let name: String
+    let sprites: Sprites
+}
+
+struct Ability: Codable {
+    let ability: AbilityInfo
+    
+}
+
+struct AbilityInfo: Codable {
+    let name: String
+    let url: String
+    
+}
+
+struct Sprites: Codable {
+    let frontDefult: String
+    
+    enum CodingKeys: String, CodingKey {
+        case frontDefault = "front_default"
+    }
+}
+
+struct PokemonType: Codable {
+    let type: Type
+}
+
+struct Type: Codable {
+    let name: String
+    let url: String
+}
+
+struct PokedexSearch: Codable {
+    let results: [Pokemon]
+}
